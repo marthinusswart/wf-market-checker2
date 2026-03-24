a62c44104e53130ad6cfb7bed65d40de74c477e3
Marthinus Swart <marthinus.swart@gmail.com>
2026-03-24 21:12:19 +1100

    Refactor main function to improve readability and comment out unused code

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ async def main():
     try:
         client = WFMClient()
         version = await client.get_version()
-        print(f"Current API version: {version}")
-        
+        print(f"Current API version: {version}")                
         item_slug = "arcane_ice"
+        
+        '''
         item_data = await client.get_item(item_slug)
         print(f"Item data for {item_slug}: {item_data}")
+        '''
         
         orders = await client.get_order(item_slug)
-        print(f"Orders for {item_slug}: {orders}")
+        #print(f"Orders for {item_slug}: {orders}")
+        
+        for order in orders:
+            print(f"Item: {item_slug}, User: {order.user.ingame_name}, Price: {order.platinum}, Status: {order.user.status}")
         
     except httpx.HTTPStatusError as e:
         print(f"An HTTP error occurred: {e}")
